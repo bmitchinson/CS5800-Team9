a03304a99fee19392b343fd06a728d930c144377
Greg Mich <gmich@smartscripts.com>
2019-10-06 13:48:15 -0500

    added schema changes and configurations. New entities also added (Registration and Instructor)

--- backend/Data/Models/Instructor.cs
+++ backend/Data/Models/Instructor.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
-using System;
 
 namespace backend.Data.Models
 {
-    public class Student
+    public class Instructor
     {
-        public int StudentId { get; set; }
+        public int InstructorId { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
 
         public ICollection<Registration> Registrations { get; set; }
     }

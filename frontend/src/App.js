2f6db3296567fa3427ad59c7fb1a096b1c349517
Ben Mitchinson <mitchinson.dev@gmail.com>
2019-10-29 21:38:33 -0500

    JWT variable routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
@@ -22,15 +23,54 @@ let theme = createMuiTheme({
 });
 theme = responsiveFontSizes(theme);
 
+const useStateWithLocalStorage = localStorageKey => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(localStorageKey) || ""
+  );
+  React.useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+  return [value, setValue];
+};
+
+// include this line if you'd like to clear JWT data
+// localStorage.removeItem("userJWT");
+
+const setDummyJWT = (userJWT, setUserJWT) => {
+  if (!userJWT) {
+    setUserJWT("active-jwt-here");
+  }
+};
+
 function App() {
+  const [userJWT, setUserJWT] = useStateWithLocalStorage("userJWT");
+
+  // setDummyJWT(userJWT, setUserJWT);
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header />
-      <MainView>
-        <StudentIndex />
-      </MainView>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <MainView>
+          {!userJWT && (
+            <Switch>
+              <h2>gotta sign in</h2>
+            </Switch>
+          )}
+          {userJWT && (
+            <Switch>
+              <Route path="/students">
+                <StudentIndex />
+              </Route>
+              <Route>
+                <h2>Home</h2>
+              </Route>
+            </Switch>
+          )}
+        </MainView>
+      </ThemeProvider>
+    </Router>
   );
 }
 

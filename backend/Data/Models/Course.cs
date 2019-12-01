c08e45e4bc64c17b08149c25e09fe35e08612843
Greg Mich <gmich@uiowa.edu>
2019-11-30 22:42:40 -0600

    fixed course controller, added soft deleted field (does not actually delete from database just adds boolean flag to filter out results), created query objects for the course queries with nested registrations and other related data

--- backend/Data/Models/Course.cs
+++ backend/Data/Models/Course.cs
@@ -3,6 +3,8 @@ using System;
 using backend.Data.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Data.Models
 {
@@ -10,20 +12,29 @@ namespace backend.Data.Models
     {
         public int CourseId { get; set; }
 
+        [Required]
         public string CourseName { get; set; }
 
+        [Required]
         public int CreditHours { get; set; }
 
+        [Required]
         public string Section { get; set; }
 
+        [Required]
         public DateTime StartTime { get; set; }
 
+        [Required]
         public DateTime EndTime { get; set; }
 
+        [Required]
         // this tag converts the enum value into a string to the frontend
         [JsonConverter(typeof(StringEnumConverter))]
         public TopicLevel Level { get; set; }
 
         public ICollection<Registration> Registrations { get; set; }
+
+        [BindNever]
+        public bool SoftDeleted { get; set; }
     }
 }
